be1bf57f4f9c3e1de55a8e7d70d4fec90cdd963d
ozzythecoder <august.chm@gmail.com>
2026-01-28 08:49:32 -0600

    main page; about & project page placeholders

---
+++ src/routes/(pages)/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Gutter from "$src/lib/components/Gutter.svelte";
+    import Nav from "$src/lib/components/Nav.svelte";
+    import SiteLayout from "$src/lib/components/SiteLayout.svelte";
+    import type { Snippet } from "svelte";
+
+    interface Props {
+        children: Snippet;
+    }
+    let { children }: Props = $props();
+</script>
+
+<SiteLayout>
+    <Gutter>
+        <Nav />
+        {@render children()}
+    </Gutter>
+</SiteLayout>
---
+++ src/routes/(pages)/about/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Head from "$src/lib/components/Head.svelte";
+    import Main from "$src/lib/components/Main.svelte";
+
+    const TITLE = "About";
+</script>
+
+<Head title={TITLE.toLowerCase()} />
+<Main>
+    <h1 class="h1">{TITLE}</h1>
+    <p>Under construction</p>
+</Main>
