59f4835c7a753e0465d6e67b1b82b31f6facb2eb
ozzythecoder <august.chm@gmail.com>
2026-01-28 10:41:42 -0600

    added tags, tag links, and descriptions to blog post page

--- src/routes/(pages)/blog/[slug]/+page.svelte
+++ src/routes/(pages)/blog/[slug]/+page.svelte
@@ -11,9 +11,26 @@
 <Head title={data.metadata.title} />
 
 <Main>
-    <h1 class="h1">{data.metadata.title}</h1>
-    <date>{formatDate(data.metadata.date as string)}</date>
-    <Prose>
-        <data.Component />
-    </Prose>
+    <section>
+        <header class="flex flex-col gap-1">
+            <h1 class="h1">{data.metadata.title}</h1>
+            <date class="italic text-surface-200"
+                >{formatDate(data.metadata.date)}</date
+            >
+            <nav class="flex flex-row gap-1 flex-wrap">
+                {#each data.metadata.tags as tag}
+                    <a
+                        href={`/blog?tag=${tag}`}
+                        class="chip preset-filled-surface-200-800"
+                    >
+                        {tag}
+                    </a>
+                {/each}
+            </nav>
+            <p class="h4 text-surface-200">{data.metadata.description}</p>
+        </header>
+        <Prose>
+            <data.Component />
+        </Prose>
+    </section>
 </Main>
