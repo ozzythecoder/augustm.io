bf133f7e304bc70faf3385e96ff7ed92cb778a96
ozzythecoder <august.chm@gmail.com>
2026-01-28 08:45:46 -0600

    metadata & main components

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    interface Props {
+        title?: string;
+        noConcat?: boolean;
+    }
+
+    const { title, noConcat = false }: Props = $props();
+
+    const pageTitle = $derived(
+        title ? (noConcat ? title : `${title} | augustm`) : "augustm",
+    );
+</script>
+
+<svelte:head>
+    <title>{pageTitle}</title>
+</svelte:head>
---
+++ src/lib/components/Main.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    interface Props {
+        children: Snippet;
+    }
+    let { children }: Props = $props();
+</script>
+
+<main id="main" tabindex="-1">
+    {@render children()}
+</main>
