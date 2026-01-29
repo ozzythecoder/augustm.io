20980568b64c422689af4be310b604f5c8bcda5b
ozzythecoder <august.chm@gmail.com>
2026-01-28 21:56:29 -0600

    silly construction images again

--- src/routes/(pages)/projects/+page.svelte
+++ src/routes/(pages)/projects/+page.svelte
@@ -10,27 +10,27 @@
     <h1 class="h1">{TITLE}</h1>
     <h3 class="h3">Under construction</h3>
     <img
-        src="http://textfiles.com/underconstruction/CoCollegeParkCampus2836construction.gif"
+        src="/images/pickaxe_construction.gif"
         alt=""
         class="inline"
     />
     <img
-        src="http://textfiles.com/underconstruction/mamagnolia_acresunderconstruction.gif"
+        src="/images/rotating_construction.gif"
         alt=""
         class="inline"
     />
     <img
-        src="http://textfiles.com/underconstruction/Dungeon4206underconstruction_004.gif"
+        src="/images/small_construction.gif"
         alt=""
         class="inline"
     />
     <img
-        src="http://textfiles.com/underconstruction/Dungeon4206underconstruction_004.gif"
+        src="/images/small_construction.gif"
         alt=""
         class="inline"
     />
     <img
-        src="http://textfiles.com/underconstruction/Dungeon4206underconstruction_004.gif"
+        src="/images/small_construction.gif"
         alt=""
         class="inline"
     />
