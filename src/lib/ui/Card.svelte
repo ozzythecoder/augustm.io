22d4b97aa2be0cadd28fcbe1b2396cc244d2c113
ozzythecoder <august.chm@gmail.com>
2026-04-15 17:07:47 -0500

    feat: added support for programmatic navigation

--- src/lib/ui/Card.svelte
+++ src/lib/ui/Card.svelte
@@ -1,45 +1,69 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import type { Snippet } from "svelte";
+    import type { HTMLAttributes } from "svelte/elements";
 
-    interface Props {
-        href?: string;
-        title: string;
+    type Props = {
+        titleClass?: string;
         className?: string;
+        href?: string;
+        title?: string;
         children: Snippet;
-    }
-    const { href, title, className, children }: Props = $props();
+    } & HTMLAttributes<HTMLDivElement>;
+    const {
+        href,
+        title,
+        titleClass,
+        className,
+        children,
+        ...restProps
+    }: Props = $props();
 
     // buncha code to make block link accessible & allow nested links
-    // 
     let cardEl: HTMLDivElement;
-    let linkEl = $state<HTMLAnchorElement>() 
+    let linkEl = $state<HTMLAnchorElement>();
 
     $effect(() => {
-        function clickLink() {
+        
+        function clickLink(e: MouseEvent) {
             const textIsSelected = window.getSelection()?.toString();
-            if (!textIsSelected && !!linkEl) {
-                linkEl.click();
+            if (!textIsSelected && !!linkEl && href) {
+                console.log(href)
+                // respect modifier keys and open in a new tab
+                if (e.ctrlKey || e.metaKey || e.shiftKey) {
+                    window.open(href, "_blank");
+                } else if (!href.startsWith("/")) {
+                    // open external links in a new tab
+                    window.open(href, "_blank");
+                } else {
+                    // programmatic navigation to refresh app state
+                    goto(href);
+                }
             }
         }
-        
+
         cardEl.addEventListener("click", clickLink);
-        
+
         const clickableElements = Array.from(cardEl.querySelectorAll("a"));
         clickableElements.forEach((ele) =>
-          ele.addEventListener("click", (e) => e.stopPropagation())
-        );       
-        
+            ele.addEventListener("click", (e) => e.stopPropagation()),
+        );
+
         return () => {
-            cardEl.removeEventListener("click", clickLink)
+            cardEl.removeEventListener("click", clickLink);
             clickableElements.forEach((ele) =>
-              ele.removeEventListener("click", (e) => e.stopPropagation())
-            );       
-        }
+                ele.removeEventListener("click", (e) => e.stopPropagation()),
+            );
+        };
     });
 </script>
 
-<div bind:this={cardEl} class={`card ${className} ${href ? "card-hover" : ""}`}>
-    <h2 class="h2">
+<div
+    bind:this={cardEl}
+    class={`card ${className} ${href ? "card-hover" : ""}`}
+    {...restProps}
+>
+    <h2 class={titleClass ?? "h2"}>
         {#if href}
             <a {href} bind:this={linkEl}>{title}</a>
         {:else}
