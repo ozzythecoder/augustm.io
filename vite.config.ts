982462415aad6060d0bedb0a20ce7cd074569038
ozzythecoder <august.chm@gmail.com>
2026-07-10 11:17:34 -0500

    explicit localhost for firefox

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
     plugins: [tailwindcss(), sveltekit()],
+    server: {
+        host: '127.0.0.1',
+        port: 5175,
+    },
     test: {
         expect: { requireAssertions: true },
         projects: [
