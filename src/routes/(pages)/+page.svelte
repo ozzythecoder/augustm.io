c08617adbb025b4944951dc887476c0cc0191101
ozzythecoder <august.chm@gmail.com>
2026-07-13 21:54:26 -0800

    add pixel art me to home page

--- src/routes/(pages)/+page.svelte
+++ src/routes/(pages)/+page.svelte
@@ -6,6 +6,7 @@
 <Head ogDescription="I make things for humans." />
 <section class="grow-0 mt-48 sm:mt-0 sm:grow grid place-items-center">
     <header class="flex flex-col items-center justify-center gap-4">
+        <img src="./me_transparent.gif" alt="" width={160} />
         <AnimatedTitle />
         <p class="text-xl sm:text-2xl tracking-wide text-text-200 font-mono">
             i make things for humans.
